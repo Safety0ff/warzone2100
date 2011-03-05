1bd78b75676b6daf2a8469d6992c4d0d55155c0b
safety0ff <safety0ff@users.sourceforge.net>
2011-03-05 00:04:37 -0500

    Use PRIu8 ("u") instead of hhu since Mingw doesn't support hhu and hhd C99 format specifiers.

--- src/keybind.cpp
+++ src/keybind.cpp
@@ -675,7 +675,7 @@ void	kf_TileInfo(void)
 {
 	MAPTILE	*psTile = mapTile(mouseTileX, mouseTileY);
 
-	debug(LOG_ERROR, "Tile position=(%d, %d) Terrain=%d Texture=%u Height=%d Illumination=%hhu",
+	debug(LOG_ERROR, "Tile position=(%d, %d) Terrain=%d Texture=%u Height=%d Illumination=%"PRIu8,
 	      mouseTileX, mouseTileY, (int)terrainType(psTile), TileNumber_tile(psTile->texture), psTile->height,
 	      psTile->illumination);
 	addConsoleMessage("Tile info dumped into log", DEFAULT_JUSTIFY, SYSTEM_MESSAGE);
