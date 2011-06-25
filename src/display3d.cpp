282acebe9417a43a38aff5fb0bc96085ec23b576
safety0ff <safety0ff@users.sourceforge.net>
2011-06-25 16:44:41 +0000

    Clean up some structure rendering logic.

--- src/display3d.cpp
+++ src/display3d.cpp
@@ -1411,8 +1411,7 @@ void displayStaticObjects( void )
 				if (psStructure->visible[selectedPlayer] || demoGetStatus())
 				{
 					if ( psStructure->pStructureType->type == REF_RESOURCE_EXTRACTOR &&
-						psStructure->psCurAnim == NULL &&
-						(psStructure->currentBuildPts > (SDWORD)psStructure->pStructureType->buildPoints) )
+						psStructure->psCurAnim == NULL && psStructure->status == SS_BUILT )
 					{
 						psStructure->psCurAnim = animObj_Add( psStructure, ID_ANIM_DERIK, 0, 0 );
 					}
@@ -1427,8 +1426,7 @@ void displayStaticObjects( void )
 					else
 					{
 						//check not a resource extractors
-						if (psStructure->pStructureType->type !=
-							REF_RESOURCE_EXTRACTOR)
+						if (psStructure->pStructureType->type != REF_RESOURCE_EXTRACTOR)
 						{
 							displayAnimation( psAnimObj, false );
 						}
