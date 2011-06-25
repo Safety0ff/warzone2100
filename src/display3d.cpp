1df4ae70208b9226792fcc29bcc3a8552b99004b
safety0ff <safety0ff@users.sourceforge.net>
2011-06-25 15:39:08 +0000

    Remove useless variable.

--- src/display3d.cpp
+++ src/display3d.cpp
@@ -1362,7 +1362,6 @@ void displayStaticObjects( void )
 {
 	STRUCTURE	*psStructure;
 	UDWORD		clan;
-	UDWORD		test = 0;
 	ANIM_OBJECT	*psAnimObj;
 
 	// to solve the flickering edges of baseplates
@@ -1375,7 +1374,6 @@ void displayStaticObjects( void )
 		for(psStructure = apsStructLists[clan]; psStructure != NULL;
 			psStructure = psStructure->psNext)
 		{
-			test++;
 			/* Worth rendering the structure? */
 			if(clipXY(psStructure->pos.x,psStructure->pos.y))
 			{
